97acc70675c2f5fbfd39ca9200c8bad024067ae8
eka jacin <1207050032@uinsgd.ac.id>
2022-11-06 06:51:28 +0700

    tampilan 1 dan log button click

---
+++ app/src/main/java/lat/pam/pizzarestaurant/MainActivity.java
@@ -0,0 +1,23 @@
+package lat.pam.pizzarestaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Tampilan_2 (View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+    }
+}

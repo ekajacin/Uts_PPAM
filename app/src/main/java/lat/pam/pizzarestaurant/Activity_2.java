82f4254310a0b5fd06295b8c13c90f4a624783aa
eka jacin <1207050032@uinsgd.ac.id>
2022-11-06 09:55:07 +0700

    tampilan selesai

--- app/src/main/java/lat/pam/pizzarestaurant/Activity_2.java
+++ app/src/main/java/lat/pam/pizzarestaurant/Activity_2.java
@@ -1,8 +1,12 @@
 package lat.pam.pizzarestaurant;
 
+import static androidx.core.content.PackageManagerCompat.LOG_TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class Activity_2 extends AppCompatActivity {
 
@@ -10,5 +14,8 @@ public class Activity_2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+    }
+    public void Tampilan_2 (View view) {
+        Log.d(LOG_TAG, "Button clicked!");
     }
 }
